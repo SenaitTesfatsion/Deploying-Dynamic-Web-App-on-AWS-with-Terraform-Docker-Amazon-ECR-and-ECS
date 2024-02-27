e4c1c152f566a731a601b24bb8f28c395aab3903
Senait Tesfatsion <tesfatsionsenait.h@gmail.com>
2024-02-26 20:42:59 -0500

    created security group file

--- variables.tf
+++ variables.tf
@@ -54,3 +54,48 @@ variable "private_data_subnet_az2_cidr" {
   description = "private data subnet az2 cidr block"
   type        = string 
 }
+
+#security groups variables
+variable "ssh_location" {
+  description = "ssh ip address"
+  type        = string 
+}
+
+
+#rds variables
+variable "database_snapshot_identifier" {
+  description = "database snapshot name"
+  type        = string 
+}
+
+variable "database_instance-class" {
+  description = "database instance type"
+  type        = string 
+}
+
+
+variable "database_instance-identifier" {
+  description = "database instance identifier"
+  type        = string 
+}
+
+variable "multi_az_deployment" {
+  description = "create a standby db instance"
+  type        = bool
+}
+
+#acm variables
+
+variable "domain_name" {
+  description = "domain name"
+  type        = string
+}
+
+variable "alternative_names" {
+  description = "sub domain name"
+  type        = string
+}
+
+
+
+
